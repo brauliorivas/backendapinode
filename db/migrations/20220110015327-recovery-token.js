0d32441b79c391356f2752a6930426fbec2ff637
Braulio Rivas Abad <brauliorivas2004@gmail.com>
2022-01-09 21:49:25 -0500

    deploy

--- db/migrations/20220110015327-recovery-token.js
+++ db/migrations/20220110015327-recovery-token.js
@@ -1,6 +1,8 @@
 'use strict';
 
 const { USER_TABLE } = require('./../models/user.model');
+const { PRODUCT_TABLE } = require('./../models/product.model');
+const { CATEGORY_TABLE } = require('./../models/category.model');
 
 module.exports = {
     up: async (queryInterface, Sequelize) => {
@@ -17,13 +19,15 @@ module.exports = {
          */
     },
 
-  down: async (queryInterface, Sequelize) => {
+    down: async (queryInterface, Sequelize) => {
         /**
          * Add reverting commands here.
          *
          * Example:
          * await queryInterface.dropTable('users');
          */
-         await queryInterface.removeColumn(USER_TABLE, 'recovery_token');
+        await queryInterface.removeColumn(USER_TABLE, 'recovery_token');
+        await queryInterface.dropTable(PRODUCT_TABLE);
+        await queryInterface.dropTable(CATEGORY_TABLE);
     }
 };
